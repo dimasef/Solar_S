d57c99bcc0e7ec892adc9d82e22d098fc014a22d
Dima <dimasef95@gmail.com>
2014-12-01 21:17:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-echo =" ewfwe"
+<?php
+// Предположим, что текущая дата March 10th, 2001, 5:16:18 pm
+
+$today = date("F j, Y, g:i a");                 // March 10, 2001, 5:16 pm
+$today = date("m.d.y");                         // 03.10.01
+$today = date("j, n, Y");                       // 10, 3, 2001
+$today = date("Ymd");                           // 20010310
+$today = date('h-i-s, j-m-y, it is w Day z ');  // 05-16-17, 10-03-01, 1631 1618 6 Fripm01
+$today = date('\i\t \i\s \t\h\e jS \d\a\y.');   // It is the 10th day.
+$today = date("D M j G:i:s T Y");               // Sat Mar 10 15:16:08 MST 2001
+$today = date('H:m:s \m \i\s\ \m\o\n\t\h');     // 17:03:17 m is month
+$today = date("H:i:s");                         // 17:16:17
+?>
