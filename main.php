47df53cf38979da942ed5175a76463df73d95fc5
Dima <dimasef95@gmail.com>
2014-12-03 00:18:41 +0200

    Update main.php

--- main.php
+++ main.php
@@ -7,6 +7,15 @@ $login=$_SESSION['log'];
 	<meta charset="utf-8">
 	<title>Solar System</title>
 	<link href="css/solar_system.css" rel="stylesheet" type="tex/css">
+  <style>
+   .name_in_header{
+  color: white;
+  padding-right: 15px;
+ }
+ .nah{
+  margin-top: 65px;
+ }
+  </style>
 </head>
 <body>
 
@@ -24,18 +33,18 @@ $login=$_SESSION['log'];
         <div id="navbar" class="navbar-collapse collapse">
           <?php
           if (empty($_SESSION['log']))
-         echo " <form class=\"navbar-form navbar-right\" role=\"form\" id=\"form1\" name=\"form1\" method=\"post\" action=\"php\\login.php\">
+         echo " <form class='navbar-form navbar-right' role=\"form\" id=\"form1\" name=\"form1\" method=\"post\" action=\"php\\login.php\">
             <div class=\"form-group\">
               <input type=\"text\" name=\"username\" placeholder=\"User name\" class=\"form-control\" required>
             </div>
             <div class=\"form-group\">
               <input type=\"password\" placeholder=\"Password\" name=\"password\" class=\"form-control\"required>
             </div>
-            <button type=\"submit\" class=\"btn btn-success\" name=\"Login\">Войти</button>
+            <button type='submit' class='btn btn-success' name='Login'>Войти</button>
           </form>";
           else 
           {
-            echo "<a href =\"php\\logout.php\" class=\"btn btn-success\">Logout</a>";
+            echo "<form class='navbar-form navbar-right'><span class='name_in_header'>Добро пожаловать, Вы $login </span><a href = 'php\\logout.php'><button type='submit' class='btn btn-success'>Выйти</button></a></form>";
           }
           ?>
         </div><!--/.navbar-collapse -->
@@ -44,10 +53,10 @@ $login=$_SESSION['log'];
     <?php
      if (!empty($_SESSION['log']))
       {
-        echo "<h2>Это главная страница ёпта!</h2>
+        echo "<div class='nah'><h2>Это главная страница ёпта!</h2>
         <h3>О да я вижу ты $login</h3>";
         if ($_SESSION['ac_lvl']==3)
-        echo "<h4>Это может видеть только админ</h4>";
+        echo "<h4>Это может видеть только админ</h4></div>";
       }
     ?>
 </body>
