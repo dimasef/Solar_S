0b0f956a38a0cae0495467665f740d0e81e92530
Dima <dimasef95@gmail.com>
2014-12-05 19:46:16 +0200

    Update header.php
    новый дизайн авторизации + меню в хедере

--- header.php
+++ header.php
@@ -8,8 +8,27 @@ $login=$_SESSION['log'];
   <title>Solar System</title>
   <link href="css/solar_system.css" rel="stylesheet" type="tex/css">
   <link href="css/styles.css" rel="stylesheet" type="tex/css">
+  <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+   <script type="text/javascript" src="js/bootstrap.js"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
 </head>
+</head>
+
 <body>
+  <script>
+$(document).ready(function(){
+
+$('#login-trigger').click(function(){
+  $(this).next('#login-content').slideToggle();
+  $(this).toggleClass('active');          
+  
+  if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+    else $(this).find('span').html('&#x25BC;')
+  })
+});
+
+  </script>
 
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
@@ -21,24 +40,48 @@ $login=$_SESSION['log'];
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="main.php">Project name</a>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Главная</a></li>
+            <li><a href="#about">Новости</a></li>
+            <li><a href="#contact">Энциклопедия</a></li>
+               <li><a href="#gelery">Галерея</a></li>
+           
+          </ul>
         </div>
+
         <div id="navbar" class="navbar-collapse collapse">
           <?php
           if (empty($_SESSION['log']))
-         echo " <form class='navbar-form navbar-right' role=\"form\" id=\"form1\" name=\"form1\" method=\"post\" action=\"login.php\">
-            <div class=\"form-group\">
-              <input type=\"text\" name=\"username\" placeholder=\"User name\" class=\"form-control\" required>
-            </div>
-            <div class=\"form-group\">
-              <input type=\"password\" placeholder=\"Password\" name=\"password\" class=\"form-control\"required>
-            </div>
+            echo "
+<nav>
+  <ul>
+    <li id='login'>
+      <a id='login-trigger' class='btn btn-success href='#'>Войти<span class='size_icon'>&#x25BC;</span>
+      </a>
+      <div id='login-content'>
+        <form class='navbar-form navbar-right' role='form' id='form1' name='form1' method='post' action='login.php'>
+          <fieldset id='inputs'>
+         
+            <input id='username' class='form-control first_top' type='text' name='username' placeholder='User name' required>  
+
+             
+            <input id='password' class='form-control' type='password' name='password' placeholder='Password' required>
+
+          </fieldset>
+          <fieldset id='actions'>
             <button type='submit' class='btn btn-success' name='Login'>Войти</button>
-            <a href = 'registration.php' class='btn btn-success btn_registr'>Регистрация</a>
-          </form>";
+             <a href='registration.php' class='btn btn-success btn_registr'>Регистрация</a>
+          </fieldset>
+        </form>
+      </div>                     
+    </li>
+  </ul>
+</nav>";
+      
           else 
           {
             echo "<form class='navbar-form navbar-right'><span class='name_in_header'>Добро пожаловать, Вы $login </span><a href = 'logout.php' class='btn btn-success'>Выйти
-            </a><a href = 'profile.php' class='btn btn-success'>Профиль</a></form>";
+            </a><a href = 'profile.php' class='btn btn-primary1 btn-primary'>Профиль</a></form>";
           }
           ?>
         </div><!--/.navbar-collapse -->
@@ -51,5 +94,3 @@ $login=$_SESSION['log'];
        echo"<div class='nah'><div class='alert alert-danger' role='alert'>Неправильный логин или пароль.</div></div>"; 
       }
     ?>
-
-
