f1b4336c5a2b9aebf966dfcbb06677a71ae447ae
Dima <dimasef95@gmail.com>
2014-12-06 13:43:52 +0200

    Update main.php

--- main.php
+++ main.php
@@ -6,6 +6,17 @@ session_start();
 	<title>Главная</title>
 </head>
 <body>
+	<style>
+.alert-danger {
+opacity: 0.7;
+top: 56px;
+width: 100%;
+position: absolute;
+color: #a94442;
+background-color: #f2dede;
+border-color: #ebccd1;
+}
+	</style>
 	<?php
 	unset($_SESSION['page']);
 	include "header.php";
