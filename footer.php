2212373cedf1288fd4d4e24df8d3ff3abd8e369a
Dima <dimasef95@gmail.com>
2014-12-07 17:47:44 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Solar System © 2014 Україна <br>
+Єфімов Дмитро та Душутін Владистав</p>
+      </div>
+    </footer>
